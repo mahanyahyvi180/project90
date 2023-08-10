7082a7429b5adab654770ee50bf334c8729b7414
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-10 15:42:51 +0330

    Add files via upload

---
+++ ConsoleApp90/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp90
+{
+    class Student
+    {
+        public string Name { get; set; }
+        private int average;
+
+        public Student(string studentName, int studentAverage)
+        {
+            Name = studentName;
+            Average = studentAverage;
+        }
+
+        public int Average
+        {
+            get
+            {
+                return average;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    if (value <= 100)
+                    {
+                        average = value;
+                    }
+                }
+            }
+        }
+        public string letterGrade
+        {
+            get
+            {
+
+                string letterGrade = string.Empty;
+
+                if (average >= 90)
+                {
+                    letterGrade = "A";
+                }
+                else if (average >= 80)
+                {
+                    letterGrade = "B";
+                }
+                else if (average >= 70)
+                {
+                    letterGrade = "C";
+                }
+                else if (average >= 60)
+                {
+                    letterGrade = "D";
+                }
+                else
+                {
+                    letterGrade = "F";
+                }
+
+                return letterGrade;
+            }
+        }
+    
+    }
+}
---
+++ ConsoleApp90/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp90
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int gradeCounter = 1;
+
+            while (gradeCounter <= 10)
+            {
+                Console.Write("Enter grade :");
+                int grade = int.Parse(Console.ReadLine());
+                total = total + grade;
+                gradeCounter = gradeCounter + 1;
+            }
+
+            int average = total / 10;
+
+            Console.WriteLine($"\nTotal of all 10 grades is{total}");
+            Console.WriteLine($"Class average is{average}");
+            Console.ReadLine();
+
+        }
+    }
+}
+
